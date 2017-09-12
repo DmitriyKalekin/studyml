2a8bf70553bc54a46f774980a155568b82981a6c
KalekinDmitry <herr_horror@mail.ru>
2017-09-12 15:41:21 +0600

    Update Simplex.py

--- Simplex.py
+++ Simplex.py
@@ -8,6 +8,7 @@
 # https://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D0%BD%D0%B5%D0%B9%D0%BD%D0%BE%D0%B5_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5
 # http://cyclowiki.org/wiki/%D0%A2%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%BD%D0%B0%D1%8F_%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0
 # http://docplayer.ru/37092434-Reshenie-zadach-lineynogo-programmirovaniya-s-ispolzovaniem-gnu-octave-glpk-i-python-a-v-ponomarev.html
+# http://www.machinelearning.ru/wiki/index.php?title=%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%9D%D0%B5%D0%BB%D0%B4%D0%B5%D1%80%D0%B0-%D0%9C%D0%B8%D0%B4%D0%B0
 
 
 
