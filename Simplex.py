ebe6f2132ab68cbbbee25ae738f4b4d6adfe9c0e
KalekinDmitry <herr_horror@mail.ru>
2017-09-12 15:39:48 +0600

    Update Simplex.py

--- Simplex.py
+++ Simplex.py
@@ -5,6 +5,11 @@
 # MustRead
 # https://habrahabr.ru/post/330648/
 # https://ru.stackoverflow.com/questions/472169/%D0%9F%D1%80%D0%BE%D1%81%D1%82%D0%B5%D0%B9%D1%88%D0%B8%D0%B9-%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1-%D1%80%D0%B5%D1%88%D0%B8%D1%82%D1%8C-%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D1%83-%D0%BF%D0%BE%D1%85%D0%BE%D0%B6%D1%83%D1%8E-%D0%BD%D0%B0-%D0%BB%D0%B8%D0%BD%D0%B5%D0%B9%D0%BD%D0%BE%D0%B5-%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5
+# https://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D0%BD%D0%B5%D0%B9%D0%BD%D0%BE%D0%B5_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5
+# http://cyclowiki.org/wiki/%D0%A2%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%BD%D0%B0%D1%8F_%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0
+# http://docplayer.ru/37092434-Reshenie-zadach-lineynogo-programmirovaniya-s-ispolzovaniem-gnu-octave-glpk-i-python-a-v-ponomarev.html
+
+
 
  
  
